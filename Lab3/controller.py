3df6870c414a2ffc2c9eb3c580795cac3e707ae2
Anairam-n3n <you@example.com>
2025-09-18 14:39:21 -0400

    adding def to Controller subclasses

--- Lab3/controller.py
+++ Lab3/controller.py
@@ -35,10 +35,35 @@ class RestaurantController(Controller):
     def create_ui(self):
         self.view.create_restaurant_ui()
 
+    def table_touched(self, table_index):
+        raise RuntimeError("table_touched: some subclasses must implement")
 
 class TableController(Controller):
-    pass
+    def __init__(self, view, restaurant, table):
+        super().__init__(view, restaurant)
+        self.table = table
+
+    def create_ui(self):
+        raise RuntimeError("create_ui: all subclasses must implement")
+
+    def seat_touched(self, seat_number):
+        raise RuntimeError("seat_touched: some subclasses must implement")
 
 
 class OrderController(Controller):
-    pass
+    def __init__(self, view, restaurant, table, seat_number):
+        super().__init__(view, restaurant)
+        self.table = table
+        self.seat_number = seat_number
+
+    def create_ui(self):
+        raise RuntimeError("create_ui: all subclasses must implement")
+
+    def add_item(self, menu_tem):
+        raise RuntimeError("add_item: some subclasses must implement")
+
+    def update_order(self):
+        raise RuntimeError("update_order: some subclasses must implement")
+
+    def cancel(self):
+        raise RuntimeError("cancel: some subclasses must implement")
